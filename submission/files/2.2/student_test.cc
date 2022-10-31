43360bce71951079396164a4f5f4cbeb7959e678
tvrctkmytfln <tvrctkmytfln@outlook.com>
2022-10-31 02:36:32 -0700

    test

--- submission/files/2.2/student_test.cc
+++ submission/files/2.2/student_test.cc
@@ -8,3 +8,18 @@
 // Write several test cases for each function.
 // Make sure you cover all corner cases!
 //-----------------------------------------------------------------------------
+TEST(Reverse, EvenSize) {
+  std::string test_string = "abcd";
+  std::cout << "test_string: " << test_string << std::endl;
+
+  Reverse(test_string);
+  EXPECT_EQ(test_string, "dcba");
+}
+
+TEST(Reverse, EvenSize2) {
+  std::string test_string = "abcd";
+  std::cout << "test_string: " << test_string << std::endl;
+
+  Reverse(test_string);
+  EXPECT_EQ(test_string, "dcba");
+}
